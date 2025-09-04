6ba27f351ed98a9d49fbbdff2d3fa6d3af013aa9
Zhaoshan-Duan <joshua.duan47@gmail.com>
2025-09-04 16:22:17 -0700

    Lambda: create event handle function

---
+++ lambda/main.go
@@ -0,0 +1,24 @@
+package main
+
+// Backend: handle all the backend logic for the application
+// 	pakcaged and zipped to be deployed to AWS Lambda function
+
+import "fmt"
+
+type MyEvent struct {
+	Username string `json:"username"`
+}
+
+// Take in a payload, process it, and return a response
+func HandlerRequest(event MyEvent) (string, error) {
+	if event.Username == "" {
+		return "", fmt.Errorf("username is cannot be empty")
+	}
+	
+	return fmt.Sprintf("Succesfully called by - %s", event.Username), nil
+}
+
+
+func main() {
+		
+}
